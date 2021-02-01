adb36784dee9b2199f266e5ebd1bee1e20d55a18
xiaohong <345125740@qq.com>
2021-02-01 18:21:27 +0800

    offer

---
+++ src/main/java/com/xh/jianzhioffer/MirrorTree.java
@@ -0,0 +1,20 @@
+package com.xh.jianzhioffer;
+
+import com.xh.tree.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author xiaohong
+ * @version 1.0
+ * @date 2021/2/1 18:04
+ * @description
+ */
+public class MirrorTree {
+
+    public TreeNode mirrorTree(TreeNode root) {
+        TreeNode newRoot = new TreeNode(root.val);
+        return newRoot;
+    }
+
+}
