46412ce1aba191c539d9bdbd5244ed879eecfa66
xh <czxqxxxh@126.com>
2021-02-24 21:11:58 +0800

    concurrent

---
+++ src/main/java/com/xh/concurrent/Print100.java
@@ -0,0 +1,47 @@
+package com.xh.concurrent;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * author  Xiao Hong
+ * date  2021/2/24 20:35
+ * description
+ * <p>
+ * 两个线程交替打印0-100
+ */
+public class Print100 {
+
+    static Semaphore semaphore1 = new Semaphore(1);
+    static Semaphore semaphore2 = new Semaphore(0);
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            for (int i = 0; i <= 100; i += 2) {
+                try {
+                    semaphore1.acquire();
+                    System.out.println(i);
+                    semaphore2.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 1; i <= 100; i += 2) {
+                try {
+                    semaphore2.acquire();
+                    System.out.println(i);
+                    semaphore1.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+
+}
