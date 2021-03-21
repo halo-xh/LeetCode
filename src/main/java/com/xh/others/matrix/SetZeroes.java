46d60c50d857a9e9eeee13570757a87b1c877bb6
xh <czxqxxxh@126.com>
2021-03-21 15:41:31 +0800

    daily

--- src/main/java/com/xh/others/matrix/SetZeroes.java
+++ src/main/java/com/xh/others/matrix/SetZeroes.java
@@ -9,11 +9,10 @@ package com.xh.others.matrix;
 
 public class SetZeroes {
     public void setZeroes(int[][] matrix) {
-        boolean x0 = false;
-        boolean y0 = false;
+        boolean x0 = false; // 记录第一行有无 0
+        boolean y0 = false; // 记录第一列有无 0
         int lengthx = matrix.length;
         int lengthy = matrix[0].length;
-
         for (int j = 0; j < lengthy; j++) {
             if (matrix[0][j] == 0) {
                 x0 = true;
@@ -26,6 +25,7 @@ public class SetZeroes {
                 break;
             }
         }
+        // 如果某个值为0  则将其对应的第一行的值置为0 对应的第一列的值置为0
         for (int i = 1; i < lengthx; i++) {
             for (int j = 1; j < lengthy; j++) {
                 if (matrix[i][j] == 0) {
@@ -34,6 +34,7 @@ public class SetZeroes {
                 }
             }
         }
+        // 根据第一行和第一列的0 将对应的行列都设置为0
         for (int i = 0; i < lengthx; i++) {
             for (int j = 0; j < lengthy; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -41,6 +42,7 @@ public class SetZeroes {
                 }
             }
         }
+        // 最后处理第一行 第一列
         if (x0) {
             for (int i = 0; i < lengthy; i++) {
                 matrix[0][i] = 0;
