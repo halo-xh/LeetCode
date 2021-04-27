e020fb0e3ec120a8eafed61af86ea01bf328e889
xiaohong <345125740@qq.com>
2021-04-27 21:04:12 +0800

    daily

---
+++ src/main/java/com/xh/others/ShipWithinDays.java
@@ -0,0 +1,43 @@
+package com.xh.others;
+
+/**
+ * @author xiaohong
+ * @version 1.0
+ * @date 2021/4/27 19:15
+ * todo.
+ * https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days/
+ */
+public class ShipWithinDays {
+
+    public int shipWithinDays(int[] ws, int d) {
+        int max = 0, sum = 0;
+        for (int w : ws) {
+            max = Math.max(max, w);
+            sum += w;
+        }
+        int l = max, r = sum;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (check(ws, mid, d)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+
+
+    boolean check(int[] ws, int t, int d) {
+        int n = ws.length;
+        int cnt = 1;
+        for (int i = 1, sum = ws[0]; i < n; sum = 0, cnt++) {
+            while (i < n && sum + ws[i] <= t) {
+                sum += ws[i];
+                i++;
+            }
+        }
+        return cnt - 1 <= d;
+    }
+
+}
