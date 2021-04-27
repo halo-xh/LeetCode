d4eb219d93c7340023db2c15341161615170f976
xiaohong <345125740@qq.com>
2021-04-27 19:03:01 +0800

    daily

---
+++ src/main/java/com/xh/tree/RangeSumBST.java
@@ -0,0 +1,33 @@
+package com.xh.tree;
+
+import java.util.Stack;
+
+/**
+ * @author xiaohong
+ * @version 1.0
+ * @date 2021/4/27 18:53
+ * @description
+ */
+public class RangeSumBST {
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode pop = stack.pop();
+            if (pop.val > high) {
+                return sum;
+            }
+            if (pop.val >= low) {
+                sum += pop.val;
+            }
+            root =pop.right;
+        }
+        return sum;
+    }
+
+}
