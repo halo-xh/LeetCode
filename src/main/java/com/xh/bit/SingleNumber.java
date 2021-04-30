842f70604e6d665777e65ab0a1b7ef05ba3d15a4
xiaohong <345125740@qq.com>
2021-04-30 09:15:26 +0800

    daily

---
+++ src/main/java/com/xh/bit/SingleNumber.java
@@ -0,0 +1,20 @@
+package com.xh.bit;
+
+/**
+ * @author xiaohong
+ * @version 1.0
+ * @date 2021/4/30 9:01
+ * <p>
+ * https://leetcode-cn.com/problems/single-number-ii/
+ */
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int a = 0, b = 0;
+        for (int num : nums) {
+            b = ~a & (b ^ num);
+            a = ~b & (a ^ num);
+        }
+        return b;
+    }
+}
