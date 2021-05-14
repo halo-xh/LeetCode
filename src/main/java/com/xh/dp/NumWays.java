649b33755c397ac5e50d73eebb98b03a58adfdf3
xiaohong <345125740@qq.com>
2021-05-14 09:33:50 +0800

    daily

---
+++ src/main/java/com/xh/dp/NumWays.java
@@ -0,0 +1,21 @@
+package com.xh.dp;
+
+/**
+ * @author xiaohong
+ * @version 1.0
+ * @date 2021/5/13 9:23
+ * <p>
+ * https://leetcode-cn.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/
+ */
+public class NumWays {
+
+    // dp
+    public int numWays(int steps, int arrLen) {
+        int[][] dp = new int[steps][arrLen];
+        dp[0][0] = 1;
+
+        return 0;
+
+
+    }
+}
---
+++ src/main/java/com/xh/others/IntToRoman.java
@@ -0,0 +1,26 @@
+package com.xh.others;
+
+/**
+ * @author xiaohong
+ * @version 1.0
+ * @date 2021/5/14 9:17
+ * <p>
+ * https://leetcode-cn.com/problems/integer-to-roman/
+ */
+public class IntToRoman {
+
+    String[] thousands = {"", "M", "MM", "MMM"};
+    String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    public String intToRoman(int num) {
+        StringBuffer roman = new StringBuffer();
+        roman.append(thousands[num / 1000]);
+        roman.append(hundreds[num % 1000 / 100]);
+        roman.append(tens[num % 100 / 10]);
+        roman.append(ones[num % 10]);
+        return roman.toString();
+    }
+
+}
