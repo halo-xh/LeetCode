add37e54182be6aad37d2ab5c68bbc3f57d13531
xiaohong <345125740@qq.com>
2021-11-04 20:35:35 +0800

    daily

--- src/main/java/com/xh/search/binarysearch/IsPerfectSquare.java
+++ src/main/java/com/xh/search/binarysearch/IsPerfectSquare.java
@@ -1,11 +1,59 @@
 package com.xh.search.binarysearch;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Xiao Hong on 2021-11-04
  * </p>
  * https://leetcode-cn.com/problems/valid-perfect-square/
  */
 public class IsPerfectSquare {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println(bs(15, arr));
+        System.out.println("---------- = ");
+        System.out.println("bs2(18) = " + bs2(808201));
+    }
+
+
+    public static boolean bs(int n, int[] arr) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (n > arr[mid]) {
+                left = mid + 1;
+            } else if (n < arr[mid]) {
+                right = mid;
+            } else {
+                return true;
+            }
+            System.out.println("left = " + left);
+            System.out.println("right = " + right);
+        }
+        return false;
+    }
+
+    public static boolean bs2(int n) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            int k = mid * mid;
+            if (n > k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+            System.out.println("k = " + k);
+            System.out.println("left = " + left);
+            System.out.println("right = " + right);
+        }
+        return false;
+    }
+
 
     public boolean isPerfectSquare(int num) {
         if (num == 1) {
@@ -16,7 +64,7 @@ public class IsPerfectSquare {
         while (left < right) {
             int mid = left + ((right - left) >> 1);
             if (mid * mid < num) {
-                left = mid +1;
+                left = mid;
             } else {
                 right = mid;
             }
