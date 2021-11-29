ef1fec409b289dc0c55ee8a741fd1fc168598be4
xiaohong <345125740@qq.com>
2021-11-29 20:46:53 +0800

    daily 简单题 我 重拳出击

--- src/main/java/com/xh/window/CheckInclusion.java
+++ src/main/java/com/xh/window/CheckInclusion.java
@@ -1,5 +1,6 @@
 package com.xh.window;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,4 +43,73 @@ public class CheckInclusion {
 
         return true;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/palindrome-number/
+     */
+    public static boolean isPalindrome(int x) {
+        String value = String.valueOf(x);
+        int length = value.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (value.charAt(i) != value.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/longest-common-prefix/
+     */
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder res = new StringBuilder();
+        String flag = null;
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            if (str.length() < minLen) {
+                minLen = str.length();
+                flag = str;
+            }
+        }
+        if (flag == null) {
+            return res.toString();
+        }
+        for (int i = 0; i < flag.toCharArray().length; i++) {
+            Character c = null;
+            for (String str : strs) {
+                c = str.charAt(i);
+                if (c != flag.charAt(i)) {
+                    return res.toString();
+                }
+            }
+            res.append(c);
+        }
+        return res.toString();
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/search-insert-position/
+     */
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length -1;
+        while (left <= right) {
+            int mid = left + ((right - left) >>> 1);
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else  {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6};
+        int i = searchInsert(arr, 3);
+        System.out.println("i = " + i);
+
+    }
+
+
 }
