b8a2997db8727a2ea86abb036bb1f8c7cdd51fa7
hongxiao <xh1005@outlook.com>
2022-03-07 20:23:59 +0800

    daily

--- src/main/java/com/xh/bit/HammingDistance.java
+++ src/main/java/com/xh/bit/HammingDistance.java
@@ -10,7 +10,7 @@ public class HammingDistance {
 
 
     public static void main(String[] args) {
-        System.out.println(1^4);
+        System.out.println(1 ^ 4);
         System.out.println(hammingDistance(1, 4));
     }
 
@@ -21,5 +21,24 @@ public class HammingDistance {
             res += 1 & (a >> i);
         }
         return res;
+    }
+
+
+    /**
+     * https://leetcode-cn.com/problems/base-7/submissions/
+     *
+     * @param num
+     * @return
+     */
+    public String convertToBase7(int num) {
+        boolean less0 = num < 0;
+        if (less0) num = -num;
+        StringBuffer buffer = new StringBuffer();
+        do {
+            buffer.append(num % 7);
+            num /= 7;
+        } while (num != 0);
+        buffer.reverse();
+        return less0 ? "-" + buffer : buffer.toString();
     }
 }
