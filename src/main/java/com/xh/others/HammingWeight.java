b2856ab88f4552d6dab77eaafaf40bd73375b9d9
hongxiao <xh1005@outlook.com>
2022-04-27 20:20:01 +0800

    daily

--- src/main/java/com/xh/others/HammingWeight.java
+++ src/main/java/com/xh/others/HammingWeight.java
@@ -3,6 +3,7 @@ package com.xh.others;
 import com.xh.common.ListNode;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * author  Xiao Hong
@@ -14,24 +15,29 @@ import java.util.*;
 public class HammingWeight {
 
     public static void main(String[] args) {
+//        HammingWeight hammingWeight = new HammingWeight();
+//        System.out.println(hammingWeight.hammingWeight(3));
+//        String s = "loveleetcode";
+//        char c = 'e';
+//        System.out.println(Arrays.toString(shortestToChar(s, c)));
+//        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+//        objectObjectHashMap.computeIfAbsent("1", k -> "123" + k);
+//        objectObjectHashMap.computeIfAbsent("2", k -> null);
+//        System.out.println("objectObjectHashMap = " + objectObjectHashMap);
+//
+//        String i = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+//        String[] split = i.split("\n");
+//        System.out.println("split = " + Arrays.toString(split));
+//
+//        System.out.println(maxRotateFunction2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+//        System.out.println("projectionArea(new int[][]{\n                {1,0},{0,2}\n        }) = " + projectionArea(new int[][]{
+//                {1, 0}, {0, 2}
+//        }));
+
+        int[][] arr = new int[][]{{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
         HammingWeight hammingWeight = new HammingWeight();
-        System.out.println(hammingWeight.hammingWeight(3));
-        String s = "loveleetcode";
-        char c = 'e';
-        System.out.println(Arrays.toString(shortestToChar(s, c)));
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
-        objectObjectHashMap.computeIfAbsent("1", k -> "123" + k);
-        objectObjectHashMap.computeIfAbsent("2", k -> null);
-        System.out.println("objectObjectHashMap = " + objectObjectHashMap);
-
-        String i = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
-        String[] split = i.split("\n");
-        System.out.println("split = " + Arrays.toString(split));
-
-        System.out.println(maxRotateFunction2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-        System.out.println("projectionArea(new int[][]{\n                {1,0},{0,2}\n        }) = " + projectionArea(new int[][]{
-                {1, 0}, {0, 2}
-        }));
+        List<List<Integer>> lists = hammingWeight.pacificAtlantic(arr);
+        System.out.println("lists = " + lists);
     }
 
     // you need to treat n as an unsigned value
@@ -225,4 +231,51 @@ public class HammingWeight {
         }
         return xy + xz + yz;
     }
+
+    private static final int[][] mv = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    //https://leetcode-cn.com/problems/pacific-atlantic-water-flow/
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] res1 = new boolean[m][n];
+        boolean[][] res2 = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dfs(heights, i, j, res1);
+                }
+                if (i == m - 1 || j == n - 1) {
+                    dfs(heights, i, j, res2);
+                }
+            }
+        }
+        List<List<Integer>> finalRes = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (res1[i][j] && res2[i][j]){
+                    finalRes.add(Arrays.asList(i,j));
+                }
+            }
+        }
+        return finalRes;
+    }
+
+    private void dfs(int[][] arr, int i, int j, boolean[][] ress) {
+        int cur = arr[i][j];
+        ress[i][j] = true;
+        for (int[] ints : mv) {
+            int x = ints[0] + i;
+            int y = ints[1] + j;
+            if (x >= arr.length || y >= arr[0].length || x < 0 || y < 0) {
+                continue;
+            }
+            if (ress[x][y] || cur > arr[x][y]){
+                continue;
+            }
+            dfs(arr, x, y, ress);
+        }
+    }
+
+
 }
