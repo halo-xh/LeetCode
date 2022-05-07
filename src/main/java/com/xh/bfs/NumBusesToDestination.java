100409f7f5f5f16738b069b6a542adb7c3a7b0ca
hongxiao <xh1005@outlook.com>
2022-05-07 18:35:15 +0800

    daily

--- src/main/java/com/xh/bfs/NumBusesToDestination.java
+++ src/main/java/com/xh/bfs/NumBusesToDestination.java
@@ -51,4 +51,53 @@ public class NumBusesToDestination {
         }
         return ret == Integer.MAX_VALUE ? -1 : ret;
     }
+
+
+    /**
+     * https://leetcode-cn.com/problems/minimum-genetic-mutation/
+     * 题目要求将一个基因序列 A 变化至另一个基因序列 B，需要满足一下条件：
+     * <p>
+     * 1。 序列 A 与 序列 B 之间只有一个字符不同；
+     * 2。 变化字符只能从 {`A', `C', `G', `T'} 中进行选择；
+     * 3。 变换后的序列 B 一定要在字符串数组 bank 中。
+     */
+    public int minMutation(String start, String end, String[] bank) {
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList(bank));
+        if (!hashSet.contains(end)) {
+            return -1;
+        }
+        if (start.equals(end)) {
+            return 0;
+        }
+        char[] keys = new char[]{'A', 'C', 'G', 'T'};
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.addLast(start);
+        int res = 1;
+        while (!linkedList.isEmpty()) {
+            int size = linkedList.size();
+            //开始处理这一层。 这一层的结果都是上一次变化的结果
+            for (int k = 0; k < size; k++) {
+                String pop = linkedList.pop();
+                for (int i = 0; i < pop.length(); i++) {
+                    for (char key : keys) {
+                        if (pop.charAt(i) != key) {
+                            StringBuilder stringBuffer = new StringBuilder(pop);
+                            stringBuffer.setCharAt(i, key);
+                            String s = stringBuffer.toString();
+                            if (hashSet.contains(s)) {
+                                if (s.equals(end)) {
+                                    return res;
+                                }
+                                linkedList.offer(s);
+                            }
+                        }
+                    }
+                }
+            }
+            // 每次队列里剩余的都是一次改变的结果。 所以处理完所有元素再加一
+            res++;
+        }
+        return -1;
+    }
+
 }
